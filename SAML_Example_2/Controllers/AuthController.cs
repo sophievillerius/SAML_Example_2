2cd0cfcf907a6a8b50c6353c19808d68a414f05a
Sophie Villerius <sophievillerius@gmail.com>
2024-04-12 13:33:32 +0200

    Create AuthController with constructor

---
+++ SAML_Example_2/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using ITfoxtec.Identity.Saml2;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace SAML_Example_2.Controllers
+{
+    [AllowAnonymous]
+    [Route("Auth")]
+    public class AuthController : Controller
+    {
+        const string relayStateReturnUrl = "ReturnUrl";
+        private readonly Saml2Configuration config;
+
+        public AuthController(IOptions<Saml2Configuration> configAccessor)
+        {
+            config = configAccessor.Value;
+        }
+    }
+}
