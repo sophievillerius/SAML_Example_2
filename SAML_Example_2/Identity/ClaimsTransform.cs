6ad8af487216b045a659e7f1b73ceba8734a322b
Sophie Villerius <sophievillerius@gmail.com>
2024-04-18 13:56:08 +0200

    Add ClaimsTransform

---
+++ SAML_Example_2/Identity/ClaimsTransform.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Claims;
+
+namespace SAML_Example_2.Identity
+{
+    public static class ClaimsTransform
+    {
+        public static ClaimsPrincipal Transform(ClaimsPrincipal incomingPrincipal)
+        {
+            if (!incomingPrincipal.Identity.IsAuthenticated)
+            {
+                return incomingPrincipal;
+            }
+
+            return CreateClaimsPrincipal(incomingPrincipal);
+        }
+
+        private static ClaimsPrincipal CreateClaimsPrincipal(ClaimsPrincipal incomingPrincipal)
+        {
+            var claims = new List<Claim>();
+
+            claims.AddRange(incomingPrincipal.Claims);
+
+            return new ClaimsPrincipal(new ClaimsIdentity(claims, incomingPrincipal.Identity.AuthenticationType, ClaimTypes.NameIdentifier, ClaimTypes.Role)
+            {
+                BootstrapContext = ((ClaimsIdentity)incomingPrincipal.Identity).BootstrapContext
+            });
+        }
+    }
+}
