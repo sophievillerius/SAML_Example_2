0b1a4def01e9ada02e0b284baf74da5bd9e941e1
Sophie Villerius <sophievillerius@gmail.com>
2024-04-18 14:13:09 +0200

    Add claims page

---
+++ SAML_Example_2/Pages/Claims.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SAML_Example_2.Pages
+{
+    public class ClaimsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
