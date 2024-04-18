3c07d5ca73ffcd83fec1dcdfd375056de24f58af
Sophie Villerius <sophievillerius@gmail.com>
2024-04-18 14:13:54 +0200

    Update Claims view

--- SAML_Example_2/Pages/Claims.cshtml.cs
+++ SAML_Example_2/Pages/Claims.cshtml.cs
@@ -1,8 +1,9 @@
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SAML_Example_2.Pages
 {
+    [Authorize]
     public class ClaimsModel : PageModel
     {
         public void OnGet()
