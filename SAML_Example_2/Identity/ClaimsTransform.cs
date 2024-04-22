9216fe86f4b32778a770ab7df25fca6c147d2b70
Sophie Villerius <sophievillerius@gmail.com>
2024-04-22 19:50:06 +0200

    Add claims for logout

--- SAML_Example_2/Identity/ClaimsTransform.cs
+++ SAML_Example_2/Identity/ClaimsTransform.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using ITfoxtec.Identity.Saml2.Claims;
+using System.Security.Claims;
 
 namespace SAML_Example_2.Identity
 {
@@ -24,6 +25,24 @@ namespace SAML_Example_2.Identity
             {
                 BootstrapContext = ((ClaimsIdentity)incomingPrincipal.Identity).BootstrapContext
             });
+        }
+
+        private static IEnumerable<Claim> GetSaml2LogoutClaims(ClaimsPrincipal principal)
+        {
+            yield return GetClaim(principal, Saml2ClaimTypes.NameId);
+            yield return GetClaim(principal, Saml2ClaimTypes.NameIdFormat);
+            yield return GetClaim(principal, Saml2ClaimTypes.SessionIndex);
+        }
+
+        private static Claim GetClaim(ClaimsPrincipal principal, string claimType)
+        {
+            return ((ClaimsIdentity)principal.Identity).Claims.FirstOrDefault(c => c.Type == claimType);
+        }
+
+        private static string GetClaimValue(ClaimsPrincipal principal, string claimType)
+        {
+            var claim = GetClaim(principal, claimType);
+            return claim != null ? claim.Value : null;
         }
     }
 }
